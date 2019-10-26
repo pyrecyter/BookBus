6a0c99e82bb1c4a7792f4a68d6193e3364944f1f
pyrecyter <sachiranuwanga123@gmail.com>
2019-10-26 17:09:47 +0530

    Updates

---
+++ precache-manifest.fb2534683728e830cb5bcc9620399c2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924db91f8177794c56d1592d533c8564",
+    "url": "/BookBus/index.html"
+  },
+  {
+    "revision": "ad8455bbdead71646350",
+    "url": "/BookBus/static/css/2.3d3bf914.chunk.css"
+  },
+  {
+    "revision": "4931d3a697f0821c11b6",
+    "url": "/BookBus/static/css/main.c6281679.chunk.css"
+  },
+  {
+    "revision": "ad8455bbdead71646350",
+    "url": "/BookBus/static/js/2.44aa07b2.chunk.js"
+  },
+  {
+    "revision": "4931d3a697f0821c11b6",
+    "url": "/BookBus/static/js/main.1307fe08.chunk.js"
+  },
+  {
+    "revision": "26e037b93202ed0123bb",
+    "url": "/BookBus/static/js/runtime-main.83b988fc.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/BookBus/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/BookBus/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/BookBus/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/BookBus/static/media/notification.c0d3c94c.eot"
+  }
+]);
